f830a6d128086434eb6dd0e31e028c152c357942
Alen Agicic <alen.agicic@learnet.se>
2024-11-08 17:14:50 +0100

    signin completed

--- AdminApp/Program.cs
+++ AdminApp/Program.cs
@@ -1,3 +1,7 @@
+using Infrastructure.Interfaces;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -5,9 +9,37 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
 
 
+<<<<<<< Updated upstream
+=======
+builder.Services.AddHttpClient();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddAuthentication(options => {
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+.AddCookie(options => {
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+    options.LoginPath = "/signin";
+    options.LogoutPath = "/signout";
+    options.AccessDeniedPath = "/denied";
+    options.SlidingExpiration = true;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+});
+
+
+builder.Services.AddScoped<IAppAuthenticationService, AuthenticationService>();
+
+
+
+
+
+>>>>>>> Stashed changes
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,11 +50,13 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+app.UseHsts();
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
