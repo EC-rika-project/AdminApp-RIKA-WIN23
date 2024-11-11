af10caeee7049e3fe2352f1cf3626ed4d72986f9
Alen Agicic <alen.agicic@learnet.se>
2024-11-11 06:15:12 +0100

    user dev grund

---
+++ AdminApp/ViewModels/UserViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Infrastructure.Dtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdminApp.ViewModels
+{
+    public class UserViewModel
+    {
+
+
+
+        public string? Id { get; set; }
+
+        [Required(ErrorMessage = "Name is Required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Firstname must be between 2 and 50 characters.")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Lastname is Required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Lastname must be between 2 and 50 characters.")]
+        public string? LastName { get; set; }
+
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+        ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is Required")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string? ConfirmPassword { get; set; }
+
+        public bool Admin { get; set; }
+
+
+
+
+        public List<UserProfileDto>? UserList { get; set; }
+
+
+
+    }
+}
---
+++ Infrastructure/Dtos/UserDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Dtos
+{
+    public class UserDTO
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? SecurityKey { get; set; }
+
+
+    }
+}
